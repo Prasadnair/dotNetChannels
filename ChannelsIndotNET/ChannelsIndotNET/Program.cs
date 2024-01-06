3a85f74bb2e3418c0febfbc57db410857c6fd6aa
Prasadnair <prasadnair44@gmail.com>
2024-01-06 08:50:45 -0500

    Channels In dotNET

---
+++ ChannelsIndotNET/ChannelsIndotNET/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
+Console.WriteLine("Channels In .NET");
+
+// Create an unbounded channel
+var channel = Channel.CreateUnbounded<int>();
+
+// Producer writing data to the channel
+async Task ProduceAsync()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        await channel.Writer.WriteAsync(i);
+        Console.WriteLine($"Produced: {i}");
+    }
+    channel.Writer.Complete();
+}
+
+// Consumer reading data from the channel
+async Task ConsumeAsync()
+{
+    while (await channel.Reader.WaitToReadAsync())
+    {
+        while (channel.Reader.TryRead(out var item))
+        {
+            Console.WriteLine($"Consumed: {item}");
+        }
+    }
+}
+
+// Run producer and consumer asynchronously
+var producerTask = ProduceAsync();
+var consumerTask = ConsumeAsync();
+
+// Wait for both tasks to complete
+await Task.WhenAll(producerTask, consumerTask);
+
+Console.ReadLine();
+
